0a05883d6c3119418323373fabcbb7211675b7fd
Developer <Lepiozdyx@gmail.com>
2025-07-23 11:20:16 -0300

    Initial Commit

---
+++ TableM/TableMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableMApp.swift
+//  TableM
+//
+//  Created by Alex on 23.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TableMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
